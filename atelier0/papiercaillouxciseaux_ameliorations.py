f8a99ffe4e30e862564242d3a1e7d11d531caa4a
Med_ <aznoggfr@gmail.com>
2023-09-07 09:42:49 +0200

    review papiercaillouxciseaux

--- atelier0/papiercaillouxciseaux_ameliorations.py
+++ atelier0/papiercaillouxciseaux_ameliorations.py
@@ -1,5 +1,7 @@
 from random import randint
 
+play_with_comp = ""
+
 # Ask to play against computer or real player
 gamemode = True
 while gamemode:
@@ -9,16 +11,15 @@ while gamemode:
     else:
         print("Je n'ai pas compris votre réponse")
 
-
 # Ask name of player 1
 player1 = input("Quel est votre nom ? ")
-print("Bienvenu ", player1, " nous allons jouer ensemble \n")
+print("Bienvenue ", player1, " nous allons jouer ensemble \n")
 player2 = 'Machine'
 
 # Ask name of player 2
 if play_with_comp == 'N':
     player2 = input("Quel est le nom du deuxième joueur ? ")
-    print("Bienvenu ", player2, " nous allons jouer ensemble \n")
+    print("Bienvenue ", player2, " nous allons jouer ensemble \n")
 
 is_playing = True
 no_round = 0
@@ -29,36 +30,39 @@ while is_playing:
     no_round += 1
 
     # Validate player1 move
-    valid_move = False
-    while valid_move == False:
-        move_player1 = input(f"{player1} faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
-        if move_player1 == 'pierre' or move_player1 == 'papier' or move_player1 == 'ciseaux':
-            valid_move = True
+    choice_player1 = ""
+    valid_choice = False
+    while not valid_choice:
+        choice_player1 = input(f"{player1} faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
+        if choice_player1 == 'pierre' or choice_player1 == 'papier' or choice_player1 == 'ciseaux':
+            valid_choice = True
         print("Je n'ai pas compris votre réponse")
 
     # Validate player2 move
     if play_with_comp == 'O':
-        move_player2 = ['pierre', 'papier', 'ciseaux'][randint(0, 2)]
+        choice_player2 = ['pierre', 'papier', 'ciseaux'][randint(0, 2)]
     else:
-        valid_move = False
-        while valid_move == False:
-            move_player2 = input(f"{player2} faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
-            if move_player2 == 'pierre' or move_player2 == 'papier' or move_player2 == 'ciseaux':
-                valid_move = True
+        choice_player2 = ""
+        valid_choice = False
+        while not valid_choice:
+            choice_player2 = input(f"{player2} faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
+            if choice_player2 == 'pierre' or choice_player2 == 'papier' or choice_player2 == 'ciseaux':
+                valid_choice = True
             print("Je n'ai pas compris votre réponse")
 
     # On affiche les choix de chacun
-    print(f"Si on récapitule : {player1} joue {move_player1} et {player2} joue {move_player2} \n")
+    print(f"Si on récapitule : {player1} joue {choice_player1} et {player2} joue {choice_player2} \n")
 
     # On regarde qui a gagné cette manche on calcule les points et on affiche le résultat
-    if move_player1 == move_player2:
+    if choice_player1 == choice_player2:
         print("le gagnant est aucun de vous, vous etes a ex æquo")
-        print(f"Les scores à l'issue de cette manche sont donc {player1}({score_player1}) et {player2}({score_player2}) \n")
+        print(
+            f"Les scores à l'issue de cette manche sont donc {player1}({score_player1}) et {player2}({score_player2}) \n")
 
-    winner = "";
-    if ((move_player1 == 'pierre' and move_player2 == 'ciseaux')
-        or (move_player1 == 'papier' and move_player2 == 'pierre')
-        or (move_player1 == 'ciseaux' and move_player2 == 'papier')):
+    winner = ""
+    if ((choice_player1 == 'pierre' and choice_player2 == 'ciseaux')
+            or (choice_player1 == 'papier' and choice_player2 == 'pierre')
+            or (choice_player1 == 'ciseaux' and choice_player2 == 'papier')):
         score_player1 += 1
         winner = player1
     else:
