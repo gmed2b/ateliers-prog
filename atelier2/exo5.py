542ea956167ec2d698a82be55926e72bf81e196c
Med_ <aznoggfr@gmail.com>
2023-09-12 16:03:31 +0200

    exo: exo 5

---
+++ atelier2/exo5.py
@@ -0,0 +1,26 @@
+def vitrines(nb_max: int, objets: list) -> (list, list):
+    """Retourne le couple de listes (vitrine1, vitrine2) tel que
+    chaque vitrine ne peut contenir deux objets identiques
+    :arg nb_max: nombre d'emplacements dans chaque vitrine
+    :arg objets: liste des objets à placer dans les vitrines
+    :return: couple de listes (vitrine1, vitrine2)
+    """
+    len_objets = len(objets)
+    if nb_max <= len_objets // 2:
+        raise ValueError("Le nombre d'objets est trop grand pour le nombre d'emplacements")
+
+    vitrine1 = []
+    nb_vitrine1 = 0
+    vitrine2 = []
+    nb_vitrine2 = 0
+    for i in range(len_objets):
+        if objets[i] not in vitrine1 and nb_vitrine1 < nb_vitrine2:
+            vitrine1.append(objets[i])
+            nb_vitrine1 += 1
+        else:
+            vitrine2.append(objets[i])
+            nb_vitrine2 += 1
+    return vitrine1, vitrine2
+
+
+def test():
