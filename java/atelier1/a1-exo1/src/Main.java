89c5c49ee061c549f8ea9152782607a2e76bcc19
Med_ <aznoggfr@gmail.com>
2023-10-04 11:39:56 +0200

    de pipe et de memoire

--- java/atelier1/a1-exo1/src/Main.java
+++ java/atelier1/a1-exo1/src/Main.java
@@ -4,20 +4,29 @@ public class Main {
     public static void main(String[] args) {
         De d1 = new De();
         De d2 = new De(12);
-        De d3 = new De("De n°1");
+        De d3 = new De("De");
+        De d4 = new DePipe(8, "Super De", 6);
+        De d5 = new DeMemoire();
         De.getNbDes();
 
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
 
+        for (int i = 0; i < 10; i++) {
         System.out.println("Lancer de d1 : " + d1.lancer());
-        System.out.println("Lancer de d2 : " + d2.lancer(10));
-        System.out.println("Lancer de d3 : " + d3.lancer());
+            System.out.println("Lancer de d2 : " + d2.lancer(10));
+            System.out.println("Lancer de d3 : " + d3.lancer());
+            System.out.println("Lancer de d4 : " + d4.lancer());
+            System.out.println("Lancer de d5 : " + d5.lancer());
+        }
 
         System.out.println("d1 == d2 : " + d1.equals(d2));
         System.out.println("d2 == d3 : " + d2.equals(d3));
         System.out.println("d1 == d3 : " + d1.equals(d3));
+        System.out.println("d1 == d5 : " + d1.equals(d5));
     }
 }
 
